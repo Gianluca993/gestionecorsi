e840bcbb974a9bc196b37994afaaea2ab695299b
Kevin <kltrz92@gmail.com>
2020-12-21 17:58:24 +0100

    Classe test AdminDAO creata

---
+++ src/it/betacom/test/dao/AdminDAOTest.java
@@ -0,0 +1,29 @@
+package it.betacom.test.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AdminDAOTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testGetById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetFactory() {
+		fail("Not yet implemented");
+	}
+
+}
