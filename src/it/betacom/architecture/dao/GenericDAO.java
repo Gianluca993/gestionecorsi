c0311887812bed9e364f949954fc3ff5742db9f6
Betacom <Betacom@Betacom-PC990C.station>
2020-12-21 15:46:50 +0100

    Interfaccia per i DAO

---
+++ src/it/betacom/architecture/dao/GenericDAO.java
@@ -0,0 +1,12 @@
+package it.betacom.architecture.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface GenericDAO<T> {
+	void create(Connection conn, T entity) throws SQLException;
+	void update(Connection conn, T entity) throws SQLException;
+	void delete(Connection conn, long id) throws SQLException;
+	T getById(Connection conn, long id) throws SQLException;
+	T[] getAll(Connection conn) throws SQLException;
+}
