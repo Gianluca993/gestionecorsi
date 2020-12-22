a81de8381a9dbb53fe383a0ed5bebc4eb47112d9
Kevin <kltrz92@gmail.com>
2020-12-22 12:18:32 +0100

    ComponentTest CorsoDAO completato

--- src/it/betacom/architecture/dao/CorsoDAO.java
+++ src/it/betacom/architecture/dao/CorsoDAO.java
@@ -1,8 +1,10 @@
 package it.betacom.architecture.dao;
 
 import java.sql.Connection;
-import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.sql.rowset.CachedRowSet;
 import javax.sql.rowset.RowSetProvider;
@@ -35,31 +37,87 @@ public class CorsoDAO implements GenericDAO<Corso>, DAOConstants{
 		rowSet.updateInt(7, entity.getAulaCorso());
 		rowSet.updateLong(8, entity.getIdDocente());
 		rowSet.insertRow();
+		rowSet.moveToCurrentRow();
 		rowSet.acceptChanges();
 	}
 
 	@Override
 	public void update(Connection conn, Corso entity) throws SQLException {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement ps;
+		ps = conn.prepareStatement(UPD_CORSO);
+		ps.setString(1, entity.getNomeCorso());
+		ps.setDate(2, new java.sql.Date(entity.getDataInizio().getTime()));
+		ps.setDate(3, new java.sql.Date(entity.getDataFine().getTime()));
+		ps.setDouble(4, entity.getCosto());
+		ps.setString(5, entity.getCommentiCorso());
+		ps.setInt(6, entity.getAulaCorso());
+		ps.setLong(7, entity.getIdDocente());
+		ps.setLong(8, entity.getIdCorso());
+		ps.execute();
+		conn.commit();
 	}
 
 	@Override
 	public void delete(Connection conn, long id) throws SQLException {
-		// TODO Auto-generated method stub
+		PreparedStatement ps;
+		ps = conn.prepareStatement(DEL_CORSO);
+		ps.setLong(1, id);
+		ps.execute();
+		conn.commit();
 		
 	}
 
 	@Override
 	public Corso getById(Connection conn, long id) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		Corso corso = null;
+		PreparedStatement ps;
+		ps = conn.prepareStatement(CORSO);
+		ps.setLong(1, id);
+		ResultSet rs = ps.executeQuery();
+		if(rs.next()) {
+			corso = new Corso();
+			corso.setIdCorso(rs.getLong(1));
+			corso.setNomeCorso(rs.getString(2));
+			corso.setDataInizio(new java.util.Date(rs.getDate(3).getTime()));
+			corso.setDataFine(new java.util.Date(rs.getDate(4).getTime()));
+			corso.setCosto(rs.getDouble(5));
+			corso.setCommentiCorso(rs.getString(6));
+			corso.setAulaCorso(rs.getInt(7));
+			corso.setIdDocente(rs.getLong(8));
+		}
+		
+		return corso;
 	}
 
 	@Override
 	public Corso[] getAll(Connection conn) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		Corso corsi[] = null;
+		Statement stmt = conn.createStatement(
+				ResultSet.TYPE_SCROLL_INSENSITIVE,
+				ResultSet.CONCUR_READ_ONLY);
+		ResultSet rs = stmt.executeQuery(CORSO_GETALL);
+		rs.last();
+		
+		
+		corsi = new Corso[rs.getRow()];
+		
+		rs.beforeFirst();
+		
+		for(int i = 0; rs.next(); i++) {
+			Corso c = new Corso();
+			c.setIdCorso(rs.getLong(1));
+			c.setNomeCorso(rs.getString(2));
+			c.setDataInizio(new java.util.Date(rs.getDate(3).getTime()));
+			c.setDataFine(new java.util.Date(rs.getDate(4).getTime()));
+			c.setCosto(rs.getDouble(5));
+			c.setCommentiCorso(rs.getString(6));
+			c.setAulaCorso(rs.getInt(7));
+			c.setIdDocente(rs.getLong(8));
+			corsi[i] = c;
+		}
+		rs.close();
+		return corsi;
+		
 	}
 
 	
--- src/test/it/betacom/architecture/dao/CorsoDAOTest.java
+++ src/test/it/betacom/architecture/dao/CorsoDAOTest.java
@@ -2,23 +2,115 @@ package test.it.betacom.architecture.dao;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Date;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import it.betacom.architecture.dao.CorsoDAO;
+import it.betacom.architecture.dao.DBAccess;
+import it.betacom.business.model.Corso;
+
 class CorsoDAOTest {
+	
+	static Connection conn;
+	static Corso corso1;
+	static Corso corso2;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		
+		conn = DBAccess.getConnection();
+		
+		corso1 = new Corso();
+		corso1.setIdCorso(1);
+		corso1.setNomeCorso("Java");
+		corso1.setDataInizio(new Date());
+		corso1.setDataFine(new Date());
+		corso1.setCosto(1000);
+		corso1.setCommentiCorso("Commenti del corso JAVA");
+		corso1.setAulaCorso(2);
+		corso1.setIdDocente(1);
+		System.out.println("CORSO 1 PER IL TEST: " + corso1.toString());
+		
+		corso2 = new Corso();
+		corso2.setIdCorso(2);
+		corso2.setNomeCorso("Angular");
+		corso2.setDataInizio(new Date());
+		corso2.setDataFine(new Date());
+		corso2.setCosto(800);
+		corso2.setCommentiCorso("Commenti del corso ANGULAR");
+		corso2.setAulaCorso(4);
+		corso2.setIdDocente(1);
+		System.out.println("CORSO 2 PER IL TEST: " + corso2.toString());
 	}
+	
+
+
+	@Test
+	void testCreate() throws SQLException, ClassNotFoundException, IOException {
+		System.out.println();
+		System.out.println("Test create()/getById()");
+		CorsoDAO corsoDAO =  CorsoDAO.getFactory();
+		corsoDAO.create(conn, corso1);
+		Corso c1 = corsoDAO.getById(conn, 1);
+		System.out.println("Corso 1 creato --> " + c1.toString());
+		
+		corsoDAO.create(conn, corso2);
+		Corso c2 = corsoDAO.getById(conn, 2);
+		System.out.println("Corso 2 creato --> " + c2.toString());
+	}
+	
+	@Test
+	void testUpdate() throws SQLException, ClassNotFoundException, IOException {
+		System.out.println();
+		System.out.println("Test update()");
+		CorsoDAO corsoDAO =  CorsoDAO.getFactory();
+		Corso c = new Corso();
+		c.setIdCorso(1);
+		c.setNomeCorso("Java 2");
+		c.setDataInizio(new Date());
+		c.setDataFine(new Date());
+		c.setCosto(1200);
+		c.setCommentiCorso("Commenti del corso JAVA 2");
+		c.setAulaCorso(5);
+		c.setIdDocente(1);
+		
+		corsoDAO.update(conn, c);
+		Corso c1 = corsoDAO.getById(conn, c.getIdCorso());
+		System.out.println("CORSO AGGIORNATO --> " + c1.toString());
+	}
+	
+	
+	@Test
+	void testGetAll() throws SQLException, ClassNotFoundException, IOException {
+		System.out.println();
+		System.out.println("Test getAll()");
+		Corso corsi[] = null;
+		CorsoDAO corsoDAO = CorsoDAO.getFactory();
+		corsi = corsoDAO.getAll(conn);
+		for(Corso c : corsi) {
+			System.out.println("CORSO: " + c.toString());
+		}
+	}
+	
+	
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
-	}
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
+		System.out.println();
+		System.out.println("Test delete()");
+		CorsoDAO corsoDAO = CorsoDAO.getFactory();
+		corsoDAO.delete(conn, 1);
+		System.out.println("Corso 1 eliminato");
+		
+		corsoDAO.delete(conn, 2);
+		System.out.println("Corso 2 eliminato");
+		conn.close();
 	}
 
 }
