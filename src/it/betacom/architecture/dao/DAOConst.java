04d4b841c1b928818faaeb94bc6169c73359c87f
Michele <michmontecchiani@gmail.com>
2020-12-22 10:28:23 +0100

    DAOConst finito

--- src/it/betacom/architecture/dao/DAOConst.java
+++ src/it/betacom/architecture/dao/DAOConst.java
@@ -13,4 +13,30 @@ public interface DAOConst {
 	String CV_DOCENTE="select cv_docente from docenti where id_docente=?";
 	String CORSO_MAXFREQ="select * from (select nome_corso ,count(id_corsista) as conteggio from corsi_corsisti, corsi where corsi_corsisti.id_corso=corsi.id_corso group by nome_corso order by conteggio desc) where rownum <=1";
 	
+	 
+	
+	//INSERT
+	String INSERT_DOC="insert into docenti values(?,?,?,?)";
+	String INSERT_CORSO="insert into corsi values(?,?,?,?,?,?,?,?)";
+	String INSERT_CORSISTA="insert into corsisti values(?,?,?,?)";
+	String INSERT_CORSCORS="insert into corsi_corsisti values(?,?)";
+	
+	//SELECT
+	String CORSISTA="select * from corsisti where id_corsista=?";
+	String DOCENTE="select * from docenti where id_docente=?";
+	String CORSO="select * from corsi where id_corso=?";
+	
+	//DELETE
+	
+	String DEL_DOC="delete from docenti where id_docente=?";
+	String DEL_CORSISTA="delete from corsisti where id_corsista=?";
+	String DEL_CORSO="delete from corsi where id_corso=?";
+	
+	//UPDATE
+	
+	String UPD_DOC="update docenti set nome_docente=?, cognome_docente=?, cv_docente=? where id_docente=? ";
+	String UPD_CORSISTA="update corsisti set nome_corsista=?, cognome_corsista=?, precedenti_formativi=? where id_corsista=?";
+	String UPD_CORSO="update corsi set nome_corso=?, data_inizio=?, data_fine=?, costo=?, commenti_corso=?, aula_corso=?, id_docente=? where id_corso=?";
+	
+	
 }
