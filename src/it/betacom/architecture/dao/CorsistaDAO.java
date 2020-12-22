2d298b2997f13071c06cbc0511c0ce4644cfd4d4
Gianluca993 <gianluca.tiribelli@gmail.com>
2020-12-22 12:54:43 +0100

    aggiunto corsista dao

--- src/it/betacom/architecture/dao/CorsistaDAO.java
+++ src/it/betacom/architecture/dao/CorsistaDAO.java
@@ -1,11 +1,17 @@
 package it.betacom.architecture.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.sql.rowset.CachedRowSet;
 import javax.sql.rowset.RowSetProvider;
 
-public class CorsistaDAO {
+import it.betacom.business.model.Corsista;
+
+public class CorsistaDAO implements GenericDAO<Corsista>{
 
 	public static CorsistaDAO getFactory() throws SQLException {
 		return new CorsistaDAO();
@@ -15,5 +21,74 @@ public class CorsistaDAO {
 	
 	private CorsistaDAO() throws SQLException {
 		rowSet = RowSetProvider.newFactory().createCachedRowSet();
+	}
+
+	@Override
+	public void create(Connection conn, Corsista entity) throws SQLException {
+		rowSet.setCommand(CORSO_GETALL);
+		rowSet.execute(conn);
+		rowSet.moveToInsertRow();
+		rowSet.updateLong(1, entity.getIdCorsista());
+		rowSet.updateString(2, entity.getNomeCorsista());
+		rowSet.updateString(3, entity.getCognomeCorsista());
+		rowSet.updateInt(4, entity.isPrecFormativi() ? 1 : 0);
+		rowSet.insertRow();
+		rowSet.moveToCurrentRow();
+		rowSet.acceptChanges();
+	}
+
+	@Override
+	public void update(Connection conn, Corsista entity) throws SQLException {
+		PreparedStatement ps;
+		ps = conn.prepareStatement(UPD_CORSISTA);
+		ps.setString(1, entity.getNomeCorsista());
+		ps.setString(2, entity.getCognomeCorsista());
+		ps.setLong(3, entity.getIdCorsista());
+		ps.execute();
+		
+	}
+
+	@Override
+	public void delete(Connection conn, long id) throws SQLException {
+		PreparedStatement ps;
+		ps = conn.prepareStatement(DEL_CORSISTA);
+		ps.setLong(3, id);
+		ps.execute();
+	}
+
+	@Override
+	public Corsista getById(Connection conn, long id) throws SQLException {
+		Corsista c = null;
+		PreparedStatement ps;
+		ps = conn.prepareStatement(CORSISTA);
+		ps.setLong(1, id);
+		ResultSet rs = ps.executeQuery();
+		if(rs.next()) {
+			c = new Corsista();
+			c.setIdCorsista(id);
+			c.setNomeCorsista(rs.getString(2));
+			c.setCognomeCorsista(rs.getString(3));
+		}
+		return c;
+	}
+
+	@Override
+	public Corsista[] getAll(Connection conn) throws SQLException {
+		Corsista cor[] = null;
+		Statement stmt = conn.createStatement(
+				ResultSet.TYPE_SCROLL_INSENSITIVE,
+				ResultSet.CONCUR_READ_ONLY);
+		ResultSet rs = stmt.executeQuery(CORSISTI_GETALL);
+		rs.last();
+		cor = new Corsista[rs.getRow()];
+		rs.beforeFirst();
+		for(int i = 0; rs.next(); i++) {
+			Corsista c = new Corsista();
+			c.setIdCorsista(rs.getLong(1));
+			c.setNomeCorsista(rs.getString(2));
+			c.setCognomeCorsista(rs.getString(3));
+			cor[i] = c;
+		}
+		return cor;
 	}
 }
--- src/test/it/betacom/architecture/dao/CorsoCorsistaDAOTest.java
+++ src/test/it/betacom/architecture/dao/CorsoCorsistaDAOTest.java
@@ -3,19 +3,39 @@ package test.it.betacom.architecture.dao;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.sql.Connection;
+import java.util.Date;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import it.betacom.architecture.dao.DBAccess;
+import it.betacom.business.model.Corsista;
+import it.betacom.business.model.Corso;
+import it.betacom.business.model.CorsoCorsista;
 
 class CorsoCorsistaDAOTest {
 	static Connection conn;
+	static Corso corso;
+	static Corsista corsista;
+	static CorsoCorsista corsoCorsista;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
-		conn = DBAccess.getConnection();
+		corso = new Corso();
+		corso.setNomeCorso("Java");
+		corso.setDataInizio(new Date());
+		corso.setDataFine(new Date());
+		corso.setCosto(1000);
+		corso.setCommentiCorso("Commenti del corso JAVA");
+		corso.setAulaCorso(2);
+		corso.setIdDocente(1);
+		System.out.println("CORSO 1 PER IL TEST: " + corso.toString());
+		
+		corsista = new Corsista();
+		corsista.setNomeCorsista("gigio");
+		corsista.setCognomeCorsista("topo");
+		corsista.setPrecFormativi(false);
 	}
 
 	@AfterAll
@@ -24,8 +44,12 @@ class CorsoCorsistaDAOTest {
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testCreateCorsoCorsista() {
+		try {
+			
+		} catch (Exception e) {
+			fail("create fallito");
+		}
 	}
 
 }
